2c211dcd6165aacfaccc3d86d21efb37605e4a71
Steveplays28 <djspaargaren@outlook.com>
2023-03-06 22:04:33 +0100

    feat: :sparkles: Mute audio when window is out of focus

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -16,8 +16,10 @@ func _ready():
 func _notification(what):
 	if (what == NOTIFICATION_APPLICATION_FOCUS_IN):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		mute_audio(false)
 	if (what == NOTIFICATION_APPLICATION_FOCUS_OUT):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		mute_audio(true)
 
 func _process(_delta):
 	set_shader_globals()
@@ -67,3 +69,18 @@ func calculate_footstep_audio(amplitude: float, sine_wave: float):
 		footsteps.pitch_scale = randf_range(0.8, 1.2)
 		footsteps.play()
 		footsteps_played = true
+
+func mute_audio(mute: bool):
+	var tween := get_tree().create_tween()
+	var tween_duration := 1.0
+
+	var master_sound_bus := AudioServer.get_bus_index("Master")
+	var set_bus_volume_db := func (volume_db, bus_idx): AudioServer.set_bus_volume_db(bus_idx, volume_db)
+	var set_bus_mute := func (enable, bus_idx): AudioServer.set_bus_mute(bus_idx, enable)
+
+	if mute:
+		tween.tween_method(set_bus_volume_db.bind(master_sound_bus), 0.0, -60.0, tween_duration)
+		tween.tween_callback(set_bus_mute.bind(master_sound_bus).bind(!mute)).set_delay(tween_duration)
+	else:
+		tween.tween_callback(set_bus_mute.bind(master_sound_bus).bind(!mute))
+		tween.tween_method(set_bus_volume_db.bind(master_sound_bus), -60.0, 0.0, tween_duration)
